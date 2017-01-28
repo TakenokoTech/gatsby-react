00cf74b6edaab3dbfa0ffc00deee835923187cbe
Takenoko <michaeelbobu@gmail.com>
2017-01-29 03:36:00 +0900

    fix sideber///

--- src/js/container/contents/Sideber.jsx
+++ src/js/container/contents/Sideber.jsx
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
-import {GaAds} from '../compornents/index.jsx';
+import {GaAds, Button} from '../compornents/index.jsx';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import panda from '../../../images/panda.png';
+import github from '../../../images/GitHub_Light.png';
 
 class Sideber extends Component {
+
+  constructor(props){
+    super(props);
+    this.onClickParent = this.onClickParent.bind(this);
+    this.state = {
+      openParent: null
+    };
+  }
+
 
   //==========================================================
   // LIFECYCLE
   //==========================================================
-  componentDidMount(){
+  componentDidMount(){}
 
+  //==========================================================
+  // RENDER
+  //==========================================================
+
+  onClickParent(i) {
+    this.setState(
+      this.state.openParent !== i ? {openParent: i} : {openParent: null}
+    );
   }
 
   //==========================================================
@@ -17,16 +37,40 @@ class Sideber extends Component {
   renderProfile() {
     return (
       <div className="_profile">
-        <div>プロフィール</div>
-        <img alt="" src=""/><div>たけのこ</div>
+        <div className="_title">Profile</div>
+        <img alt="" src={panda}/><div className="_name">たけのこ</div>
+        <div className="_clear" />
+        <div className="_link">
+          <Button className="_social _g"><img alt="" src={github} /></Button>
+          <Button className="_social _g"><img alt="" src={github} /></Button>
+          <Button className="_social _g"><img alt="" src={github} /></Button>
+          <Button className="_social _g"><img alt="" src={github} /></Button>
+          <div className="_clear" />
+        </div>
       </div>
     );
   }
 
   renderCategory() {
+    const renderChildren = (i) => {
+      return (
+        <ReactCSSTransitionGroup transitionName="categoryAnime" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+          <ul key={i} className="slidein">
+            <li><Button className="_child">カテゴリ</Button></li>
+            <li><Button className="_child">カテゴリ</Button></li>
+            <li><Button className="_child">カテゴリ</Button></li>
+          </ul>
+        </ReactCSSTransitionGroup>
+      );
+    };
     return(
       <div>
-        <div>カテゴリ</div>
+        <div className="_title">Category</div>
+        <ul className="dropmenu">
+          <li><Button className="_parent" onClick={() => this.onClickParent(0)}>親カテゴリ</Button>
+            {this.state.openParent === 0 ? renderChildren(0) : null}
+          </li>
+        </ul>
       </div>
     );
   }
@@ -34,7 +78,7 @@ class Sideber extends Component {
   renderFavor() {
     return(
       <div>
-        <div>人気</div>
+        <div className="_title">人気</div>
       </div>
     );
   }
@@ -42,7 +86,7 @@ class Sideber extends Component {
   renderRecent() {
     return(
       <div>
-        <div>最近</div>
+        <div className="_title">最近</div>
       </div>
     );
   }
@@ -53,10 +97,10 @@ class Sideber extends Component {
     return (
       <div className={className}>
         <div className=""><GaAds/></div>
-        <div className="">{this.renderProfile()}</div>
-        <div className="">{this.renderCategory()}</div>
-        <div className="">{this.renderFavor()}</div>
-        <div className="">{this.renderRecent()}</div>
+        <div className="_content">{this.renderProfile()}</div>
+        <div className="_content">{this.renderCategory()}</div>
+        <div className="_content">{this.renderFavor()}</div>
+        <div className="_content">{this.renderRecent()}</div>
       </div>
     );
   }
