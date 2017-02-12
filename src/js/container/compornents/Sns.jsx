21e3da54fab13eafefb6d62ee1d1df95236f2d8d
Takenoko <michaeelbobu@gmail.com>
2017-02-12 23:54:17 +0900

    add socialbutton....

--- src/js/container/compornents/Sns.jsx
+++ src/js/container/compornents/Sns.jsx
@@ -7,8 +7,14 @@ class Sns extends Component {
 
   }
 
-  onClick() {
+  openFacebook(url) {
+    window.open(url, 'FBwindow', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes');
+    return false;
+  }
 
+  openGoogle(url) {
+    window.open(url,'Gwindow', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes');
+    return false;
   }
 
   render() {
@@ -16,15 +22,24 @@ class Sns extends Component {
     + "z-depth-1 "
     + "waves-effect waves-light ";
 
+    const url = "http://www.google.com"//window.location.href;
+    const title = "タイトル";
+    const category = "カテゴリ";
+    // const hatebuHref = "http://b.hatena.ne.jp/entry/{" + url + "}";
+    const twitterHref = "https://twitter.com/intent/tweet?text=" + title + "&url=" + url +"&hashtags=" + category;
+    const facebookHref = "http://www.facebook.com/share.php?u=" + url;
+    const googleHref = "https://plus.google.com/share?url=" + url;
+    const linkedinHref = "http://www.linkedin.com/shareArticle?mini=true&url=" + url + "&title=" + title;
+
     return (
       <div className="sns_component">
         <div className="_sns_title">シェアする!!</div>
         <ul>
-          <li className={liClass + "hatebu"}><img alt="" src={hatelogo}/></li>
-          <li className={liClass + "twitter"}><i className="fi-social-twitter" /></li>
-          <li className={liClass + "facebook"}><i className="fi-social-facebook" /></li>
-          <li className={liClass + "googleplus"}><i className="fi-social-google-plus" /></li>
-          <li className={liClass + "linkedin"}><i className="fi-social-linkedin" /></li>
+          <a target="_blank" disabled={true}><li className={liClass + "hatebu"}><img alt="" src={hatelogo}/></li></a>
+          <a href={twitterHref} rel="nofollow" target="_blank"><li className={liClass + "twitter"}><i className="fi-social-twitter" /></li></a>
+          <a onClick={() => this.openFacebook(facebookHref)}><li className={liClass + "facebook"}><i className="fi-social-facebook" /></li></a>
+          <a onClick={() => this.openGoogle(googleHref)}><li className={liClass + "googleplus"}><i className="fi-social-google-plus" /></li></a>
+          <a href={linkedinHref} rel="nofollow" target="_blank"><li className={liClass + "linkedin"}><i className="fi-social-linkedin" /></li></a>
         </ul>
       </div>
     );
